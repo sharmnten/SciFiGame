ffdc1fd2cc7108d40b2ba3a4ce970c2b9eb877e4
sharmnten <115847442+sharmnten@users.noreply.github.com>
2024-09-11 00:57:53 +0000

    sliced runAnim

--- mygame/src/main.js
+++ mygame/src/main.js
@@ -1,13 +1,51 @@
-import kaplay from "kaplay";
+import kaplay,{} from "kaplay";
 import "kaplay/global";
 
-const k = kaplay()
+kaplay({
+    debugKey:"d",
+})
 
-k.loadSprite("bean", "sprites/bean.png")
+loadSprite("hero-run","/sprites/run.png", {
 
-k.add([
-	k.pos(120, 80),
-	k.sprite("bean"),
+        "x": 0,
+        "y": 0,
+        "width":126 ,
+        "height": 108,
+        "sliceY": 4,
+		"sliceX":1,
+        "anims": {
+            "run": {
+                "from": 0,
+                "to": 3,
+                "speed": 3,
+                "loop": true,
+            },
+            
+    },
+});
+
+
+
+
+
+const player=add([
+    sprite("hero-run"),
+    pos(width()/2,height()/2),
+	scale(2,2),
+    area(),
+    anchor("left"),
 ])
 
-k.onClick(() => k.addKaboom(k.mousePos()))
+player.play("run")
+
+onKeyDown("left", () => {
+    player.move(-100, 0),
+    player.flipX = true;
+    
+    
+})
+onKeyDown("right", () => {
+    player.move(100, 0),
+    player.flipX = false;
+    
+})
